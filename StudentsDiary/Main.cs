4dba3647afed31a5bfd3734f926c105494e27f54
Bartosz Rachwał <bartekr90@gmail.com>
2021-12-24 16:35:01 +0100

    serialization and deserialization

--- StudentsDiary/Main.cs
+++ StudentsDiary/Main.cs
@@ -3,23 +3,49 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace StudentsDiary
 {
     public partial class Main : Form
     {
+        private string _filePath = Path.Combine(Environment.CurrentDirectory,"students.txt");
+
         public Main()
         {
-            InitializeComponent();
-           
-
+            InitializeComponent();            
         }
 
-       
+        public void SerializeToFile(List<Student> students)
+        {
+            var serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (var streanWriter = new StreamWriter(_filePath))
+            {
+            serializer.Serialize(streanWriter,students);
+            streanWriter.Close();            
+            }
+        }
+
+        public List<Student> DeserializeFromFile()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Student>();
+
+            var serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (var streamReader = new StreamReader(_filePath))
+            {
+                var students = (List<Student>)serializer.Deserialize(streamReader);                
+                streamReader.Close();
+                return students;
+            }
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
